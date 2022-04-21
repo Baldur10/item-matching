970cdc91765dddd364657278c792f229aa718a17
Rahil Raj Mohan <rahilmohan12@gmail.com>
2022-04-22 02:30:42 +0800

    updated cnn file

--- utils.py
+++ utils.py
@@ -41,12 +41,60 @@ def feat_extractor_query():
         crop_resize = cv2.resize(crop, (224, 224), interpolation=cv2.INTER_CUBIC)
         return crop_resize,featsave_path
 
-def similarity(query_feat, gallery_feat):
+def visualization(retrived, query):
+    plt.subplot(4, 4, 1)
+    plt.title('query')
+    query_img = cv2.imread(query)
+    img_rgb_rgb = query_img[:,:,::-1]
+    plt.imshow(img_rgb_rgb)
+    for i in range(10):
+        img_path = './datasets_4186/gallery_4186/' + retrived[i][0]
+        img = cv2.imread(img_path)
+        img_rgb = img[:,:,::-1]
+        # img_rbg = cv2.resize(img_rbg, (224, 224), interpolation=cv2.INTER_CUBIC)
+        plt.subplot(4, 4, i+2)
+        plt.title(retrived[i][1])
+        plt.imshow(img_rgb)
+    plt.show()
+
+def retrival_idx(query_path, gallery_dir, similarityType = 'cosine'):
+    query_feat = np.load(query_path, allow_pickle=True)
+    # print(query_feat)
+    
+    dict_values = {}
+    # print(dict_values)
+    
+    for gallery_file in os.listdir(gallery_dir):
+        gallery_feat = np.load(os.path.join(gallery_dir, gallery_file),  allow_pickle=True)
+        gallery_idx = gallery_file.split('.')[0] + '.jpg'
+
+        if similarityType == 'cosine':
+            sim = similarity_cosine(query_feat, gallery_feat)
+        elif similarityType == 'pearson':
+            sim = similarity_pearson(query_feat, gallery_feat)
+        elif similarityType == 'euclidean':
+            sim = similarity_distance(query_feat, gallery_feat)
+        # print(sim)
+        dict_values[gallery_idx] = sim
+    sorted_dict = sorted(dict_values.items(), key=lambda item: item[1]) # Sort the similarity score
+    if similarityType == 'euclidean':
+        best_ten = sorted_dict[:10]
+    else:
+        best_ten = sorted_dict[-10:] # Get the best five retrived images
+    # print(best_ten)
+    return best_ten
+
+def similarity_cosine(query_feat, gallery_feat):
     sim = cosine_similarity(query_feat, gallery_feat)
     sim = np.squeeze(sim)
     return sim
+    
+def similarity_pearson(query_feat, gallery_feat):
+    sim = (np.corrcoef(query_feat, gallery_feat)[0,1])**2
+    sim = np.squeeze(sim)
+    return sim
 
-def distance_similarity(query_feat, gallery_feat):
-    sim = np.linalg.norm(query_feat, gallery_feat)
+def similarity_distance(query_feat, gallery_feat):
+    sim = np.linalg.norm(query_feat-gallery_feat)
     sim = np.squeeze(sim)
     return sim
