70136cbe0d19e258d6d46d298d1c7529957aa941
Rahil Raj Mohan <rahilmohan12@gmail.com>
2022-04-23 09:17:15 +0800

    Updated with histogram

--- utils.py
+++ utils.py
@@ -13,47 +13,41 @@ def query_crop(query_path, txt_path, save_path):
     cv2.imwrite(save_path, crop[:,:,::-1])  #save the cropped region
     return crop
 
-def feat_extractor_gallery(gallery_dir, feat_savedir):
-    for img_file in tqdm(os.listdir(gallery_dir)):
-        img = cv2.imread(os.path.join(gallery_dir, img_file))
-        img = img[:,:,::-1] #bgr2rgb
-        img_resize = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC) # resize the image
-        featsave_path = os.path.join(feat_savedir, img_file.split('.')[0]+'.npy')
-        return img_resize, featsave_path
-
-def feat_extractor_query():
-    query_dir = './datasets_4186/query_4186/'
-    txt_dir = './datasets_4186/query_txt_4186/'
-    save_dir =  './datasets_4186/query_cropped/'
-    featsave_dir = './datasets_4186/query_feature/'
-    for query_file in tqdm(os.listdir(query_dir)):
-        if query_file.endswith(".DS_Store"):
-            continue
-        print(query_file)
-        img_name = query_file[0:query_file.find('.')]
-        txt_file = img_name+'.txt'
-        featsave_file = img_name+'_feats.npy'
-        query_path = os.path.join(query_dir, query_file)
-        txt_path = os.path.join(txt_dir, txt_file)
-        save_path = os.path.join(save_dir, query_file)
-        featsave_path =os.path.join(featsave_dir, featsave_file) 
-        crop = query_crop(query_path, txt_path, save_path)
-        crop_resize = cv2.resize(crop, (224, 224), interpolation=cv2.INTER_CUBIC)
-        return crop_resize,featsave_path
-
 def visualization(retrived, query):
-    plt.subplot(4, 4, 1)
+    plt.subplot(5, 5, 1)
     plt.title('query')
     query_img = cv2.imread(query)
     img_rgb_rgb = query_img[:,:,::-1]
     plt.imshow(img_rgb_rgb)
     for i in range(10):
         img_path = './datasets_4186/gallery_4186/' + retrived[i][0]
+        # print(img_path)
         img = cv2.imread(img_path)
         img_rgb = img[:,:,::-1]
-        # img_rbg = cv2.resize(img_rbg, (224, 224), interpolation=cv2.INTER_CUBIC)
-        plt.subplot(4, 4, i+2)
-        plt.title(retrived[i][1])
+        img_rgb = cv2.resize(img_rgb, (224, 224), interpolation=cv2.INTER_CUBIC)
+        plt.subplot(5, 5, i+2)
+        plt.tight_layout(h_pad=2)
+        plt.title(round(retrived[i][1],2))
+        plt.imshow(img_rgb)
+    plt.show()
+
+def visualization_hist(retrived, query):
+    plt.subplot(5, 5, 1)
+    plt.title('query')
+    query_img = cv2.imread(query)
+    img_rgb_rgb = query_img[:,:,::-1]
+    plt.imshow(img_rgb_rgb)
+    for i in range(10):
+        # print(retrived[i][0][0:retrived[i][0].find('_')])
+        img_path = './datasets_4186/gallery_4186/' + retrived[i][0][0:retrived[i][0].find('_')] + '.jpg'
+        # print(retrived[i][0][0:retrived[i][0].find('_')])
+        # print(img_path)
+        img = cv2.imread(img_path)
+        img_rgb = img[:,:,::-1]
+        img_rgb = cv2.resize(img_rgb, (224, 224), interpolation=cv2.INTER_CUBIC)
+        plt.subplot(5, 5, i+2)
+        plt.tight_layout(h_pad=2)
+        plt.title(round(retrived[i][1],2))
         plt.imshow(img_rgb)
     plt.show()
 
